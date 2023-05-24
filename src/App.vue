f020a27ec5199d93a3f5d2d1a07a3a62376c1dc9
IreneCaiazzo <irene.caiazzo707@gmail.com>
2023-05-24 16:53:40 +0200

    basic style header/ searchbar

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,61 @@
 import HeaderApp from './components/HeaderApp.vue';
 import CardList from './components/CardList.vue';
 import Card from './components/Card.vue';
+import axios from 'axios';
+import { store } from './store';
 
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+
+  components: {
+    HeaderApp,
+    CardList,
+    Card,
+  },
+
+  created() {
+    axios.get('https://api.themoviedb.org/3/search/movie').then(response => (this.store.arrCards = response.data.data));
+  },
+}
 </script>
 
-<template></template>
+<template>
+  <header>
+    <nav>
+      <h1>Boolflix</h1>
+      <div class="search">
+        <input type="text">
+        <button>Cerca</button>
+      </div>
+    </nav>
+  </header>
+</template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 5rem;
+  background-color: black;
+  color: red;
+  padding-inline: 2rem;
+
+  input {
+    margin-right: 1rem;
+  }
+
+}
+</style>
